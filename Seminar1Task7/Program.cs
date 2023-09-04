749274523e852b7b918d73b7f924e87b6fee1b54
Vladimir Salnikov <2svladimir@gmail.com>
2023-09-04 22:01:41 +0300

    solution from seminar1 task7

---
+++ Seminar1Task7/Program.cs
@@ -0,0 +1,40 @@
+﻿//1 Анастасия
+// Console.WriteLine("Введите число:");
+// int num = int.Parse(Console.ReadLine()??"0");
+
+// Console.WriteLine(num % 10);
+
+
+// 2 Иван
+//  int N = int.Parse(Console.ReadLine()??"0");
+// if (N < 1000 && N > 99){
+//     Console.WriteLine("Последняя цифра " + N%10);
+// } else {
+//     Console.WriteLine("ошибка ввода");
+// }
+
+// 3 Артем
+// Console.WriteLine("Enter three digit number");
+//     int num = int.Parse(Console.ReadLine());
+    
+//     Console.WriteLine(num % 10);
+
+// 4 Анастасия
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine(num < 1000 && num > 99 ? num % 10 : "Число не трёхзначное");
+
+// 5 Кирил
+// System.Console.WriteLine("Введите трехзначное число:");
+// int num = int.Parse(Console.ReadLine() ?? "0");
+// int lastDigit = 0;
+// if (num >= 100 && num < 1000)
+// {
+// lastDigit = num % 10;
+// }
+// else
+// {
+// System.Console.WriteLine("Число не трехзначное");
+// }
+// System.Console.WriteLine("Последняя цифра числа: " + lastDigit);
