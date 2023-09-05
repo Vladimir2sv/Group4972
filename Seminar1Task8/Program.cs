fd9f5403042be2799bc0a8ae19bfe680f61d26a8
Vladimir Salnikov <2svladimir@gmail.com>
2023-09-05 23:15:46 +0300

    solution seminar1 task 8 project

---
+++ Seminar1Task8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает
+// все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int lastDigit = 0;
+if (num >= 100 && num < 1000)
+{
+lastDigit = num % 10;
+}
+else
+{
+System.Console.WriteLine("Число не трехзначное");
+}
+System.Console.WriteLine("Последняя цифра числа: " + lastDigit);
