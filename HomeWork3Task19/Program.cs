6d3ecce066077ba1c1398baf39e8361a9fae3465
Vladimir Salnikov <2svladimir@gmail.com>
2023-09-17 21:22:20 +0300

    solution from seminar 4 task 24, 26, 28, 30

---
+++ HomeWork3Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿char[] temp=number.ToString().ToCharArray();
+      
+      if(number.ToString().Length !=5)
+      {
+        Console.WriteLine("Число не пятизначное");
+        return false;
+      }
+      else if(temp[0]==temp[4]&&temp[1]==temp[3])
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+
+//------------------------------------------------------------
+
+string num = Convert.ToString(number);
+
+if (num.Length != 5)
+{
+    Console.WriteLine("Число не пятизначное");
+    return false;
+}
+else if (num[0] == num[4] && num[1] == num[3]) return true;
+else return false;
---
+++ Seminar4Task28/Program.cs
@@ -0,0 +1,69 @@
+﻿// No28 Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+
+// Вариант 1
+// Ввод числа из консоли
+// int readData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     int num = int.Parse(Console.ReadLine() ?? "0");
+//     return num;
+// }
+
+// // Вывод числа в консоль
+// void PrintResult(string mes)
+// {
+//     Console.WriteLine(mes);
+// }
+
+
+// long FactorialNum(int num)
+// {
+//     long fact = 1;
+
+//     for (int i = 2; i <= num; i++)
+//     {
+//         fact *= i;
+//     }
+//     return fact;
+// }
+
+// int enterNum = readData("Введите число: ");
+// long result = FactorialNum(enterNum);
+
+// PrintResult($"Произведение чисел от 1 до {enterNum} = {result}");
+// //PrintResult("Произведение чисел от 1 до " + enterNum + " = " + result);
+
+// Вариант 2
+
+// Для использования BigInteger
+using System.Numerics;
+
+// Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+// Выводим результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Считаем цифры
+BigInteger CountFact(int num)
+{
+    BigInteger fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int num = ReadData("Введите целое число: ");
+BigInteger fact = CountFact(num);
+PrintResult("Факториал числа " + num + " равен " + fact);
