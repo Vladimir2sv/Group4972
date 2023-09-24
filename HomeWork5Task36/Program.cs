7afc40d73ce3aa2cec07c3d4b6deeaee6909bdbe
Vladimir Salnikov <2svladimir@gmail.com>
2023-09-24 13:56:18 +0300

    solution from home work 5 task 34, 36, 38

---
+++ HomeWork5Task36/Program.cs
@@ -0,0 +1,52 @@
+﻿// №36 Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [3, 7, -2, 1] -> 8
+// [-4, -6, 89, 6] -> 0
+
+//Заполняем одномерный массив
+int[] GenArray(int length, int lowBorder, int highBorder)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(lowBorder, highBorder+1);
+    }
+
+    return array;
+}
+
+// Печатаем результат
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+        
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Считаем сумму элементов массива, стоящих на нечетных позициях
+int SumNegPos(int[] arr)
+{
+    int sum = 0;
+    
+    for (int i = 1; i < arr.Length; i=i+2)
+    {
+        sum = sum + arr[i];
+    }
+
+    return sum;
+}    
+
+// Выводим сообщение на консоль
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int[] array = GenArray(7, 0, 100);
+PrintArray(array);
+int sum = SumNegPos(array);
+PrintResult($"Сумма элементов, стоящих на нечётных позициях = {sum}");
---
+++ HomeWork5Task38/Program.cs
@@ -0,0 +1,54 @@
+﻿// No38 Задайте массив вещественных чисел. Найдите разницу
+// между максимальным и минимальным элементом массива.
+
+// Заполняем одномерный массив из вещественных чисел
+double[] GenArray(int length, double lowBorder, double highBorder)
+{
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().NextDouble() * (highBorder - lowBorder) + lowBorder;
+    }
+
+    return array;
+}
+
+// Печатаем результат
+void PrintArray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(string.Format("{0:0.00}", arr[i]) + ", ");
+    }
+    Console.WriteLine(string.Format("{0:0.00}", arr[arr.Length - 1]) + "]");
+}
+
+// Находим разницу между максимальным и минимальным элементом массива
+double FindDifference(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    
+    // Перебираем элементы массива и находим максимальный и минимальный элемент
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    // Разница между максимальным и минимальным элементом массива
+    return max - min;
+}
+
+double[] array = GenArray(12, -9, 9);
+PrintArray(array);
+
+double difference = FindDifference(array);
+Console.WriteLine("Разница: " + string.Format("{0:0.00}", difference));
